fde0189ed3570a18493b386a3e44906b58410d9c
woodgeon <ad4086@naver.com>
2025-08-04 15:09:27 +0900

    refactor: UserResponse 클래스를 record로 변경하여 불변성 확보

--- src/main/java/com/daylily/domain/auth/web/dto/UserResponse.java
+++ src/main/java/com/daylily/domain/auth/web/dto/UserResponse.java
@@ -1,13 +1,8 @@
 package com.daylily.domain.auth.web.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class UserResponse {
-    private Integer githubId;
-    private String login;
-    private String email;
-    private String githubProfileUrl;
-}
+public record UserResponse(
+        Integer githubId,
+        String login,
+        String email,
+        String githubProfileUrl
+) {}
