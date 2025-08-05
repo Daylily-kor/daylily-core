2c379147ff06060be9d08118c2e966bd3748f9ff
nyhryan <nyhryan99102@gmail.com>
2025-08-05 14:43:22 +0900

    chore: 🧹 GitHub App 엔티티에 `@GeneratedValue` 누락 수정

--- src/main/java/com/daylily/domain/github/entity/GitHubApp.java
+++ src/main/java/com/daylily/domain/github/entity/GitHubApp.java
@@ -12,6 +12,7 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GitHubApp {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String clientId;
