a5d7fd62c5be1b01d5a1b3c61ad7908891579113
woodgeon <ad4086@naver.com>
2025-08-19 00:57:19 +0900

    chore: CORS 설정 변경 6

--- src/main/java/com/daylily/global/config/WebConfig.java
+++ src/main/java/com/daylily/global/config/WebConfig.java
@@ -1,4 +1,20 @@
 package com.daylily.global.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")        // GET, POST, PUT, DELETE, OPTIONS 등 모든 메서드
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
 }
