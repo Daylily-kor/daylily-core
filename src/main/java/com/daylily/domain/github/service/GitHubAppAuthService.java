5f3a0907f899fb3cd9f705f38a28f60c9ab1a265
nyhryan <nyhryan99102@gmail.com>
2025-08-21 02:34:11 +0900

    debug: 🧪

--- src/main/java/com/daylily/domain/github/service/GitHubAppAuthService.java
+++ src/main/java/com/daylily/domain/github/service/GitHubAppAuthService.java
@@ -71,7 +71,7 @@ public class GitHubAppAuthService {
         jwtCookie.setMaxAge(60 * 60); // 1 hour
         jwtCookie.setHttpOnly(true);
         jwtCookie.setSecure(false);
-        jwtCookie.setDomain(".ataidev.cc");
+        jwtCookie.setDomain("ataidev.cc");
 //        jwtCookie.setAttribute("SameSite", "Lax");
         return AuthResult.success(jwtCookie);
     }
