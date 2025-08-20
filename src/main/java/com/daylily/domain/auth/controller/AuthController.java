aaf29cff11652333af464e3866762707599a697e
nyhryan <nyhryan99102@gmail.com>
2025-08-21 03:49:38 +0900

    debug: 🧪

--- src/main/java/com/daylily/domain/auth/controller/AuthController.java
+++ src/main/java/com/daylily/domain/auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.daylily.global.response.SuccessResponse;
 import com.daylily.global.response.code.ErrorCode;
 import com.daylily.global.util.StateStore;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
@@ -23,7 +25,12 @@ public class AuthController {
     @PostMapping("/exchange")
     public ResponseEntity<?> exchangeStateForJwt(
             @RequestBody StateExchangeRequest request) {
+
+        log.debug("Exchanging state for JWT: {}", request.state());
+
         String jwt = stateStore.consumeJwt(request.state().toString());
+
+        log.debug("JWT retrieved for state {}: {}", request.state(), jwt);
 
         if (jwt != null) {
             return SuccessResponse.ok(Map.of("jwt", jwt));
